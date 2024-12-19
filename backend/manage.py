bcd27a7c6cf837b93daf1c2fe6b9d3a744fb69a7
pumatopdev <puma.topdev@gmail.com>
2024-12-19 08:46:04 -0800

    Update manage.py
    manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -9,6 +9,11 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
+        ## Override default port for `runserver` command
+            #from django.core.management.commands.runserver import Command as runserver
+            #runserver.default_port = "8080"
+
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
